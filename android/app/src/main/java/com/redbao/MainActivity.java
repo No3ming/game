56ec514e9b9643c44a7de9f270ff4b16f5630c07
lidm <lidm@cloudream.com>
2018-03-16 19:14:06 +0800

    启动页面

--- android/app/src/main/java/com/redbao/MainActivity.java
+++ android/app/src/main/java/com/redbao/MainActivity.java
@@ -1,5 +1,9 @@
 package com.redbao;
-
+import android.os.Bundle; // here
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen; // here
+// react-native-splash-screen < 0.3.1
+// import com.cboy.rn.splashscreen.SplashScreen; // here
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +15,10 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "RedBao";
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
     }
 }
